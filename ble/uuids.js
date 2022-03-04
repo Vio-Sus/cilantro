81c656051638dede4288a10a860c7a1c01f1bc6a
Sam Meech-Ward <sam@meech-ward.me>
2022-03-04 15:50:34 -0800

    🤗

--- ble/uuids.js
+++ ble/uuids.js
@@ -7,10 +7,10 @@
 
 module.exports = {
   // wifiService: '3dc07bac-734a-4ea7-8923-d87cbbbcf96b',
-  deviceService: 'bca881ce-42c0-444d-91f4-b31e9acc667c',
+  deviceService: '42c0',
   readCharacteristic: 'c40d35b1-9bcd-401f-b5ee-8abf58dab24b',
   testCharacteristic: 'd2ae53f6-a180-42c1-9727-acda88487657',
-  deviceService2: '698956c1',
+  deviceService2: '9d4f',
   readCharacteristic2: 'ea22b694-cb28-4903-8f56-755df1235e0e',
   testCharacteristic2: 'e7e73221-a5f1-4b5a-8582-b299cf4a1425',
   // writeCharacteristic: 'd66bec57-6677-4631-a9bd-a9a05ef4695f'
